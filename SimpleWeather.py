5c818e095601a1da69864d8a06bdcd264b120f18
noahdev <93200455+odeyity@users.noreply.github.com>
2022-04-30 12:37:26 +0100

    Add files via upload

---
+++ SimpleWeather.py
@@ -0,0 +1,22 @@
+
+import cgitb cgitb.enable(start_response('200 OK', [('Content-Type', 'text/html')]))
+from pyowm import OWM
+from pyowm.utils import config
+from pyowm.utils import timestamps
+
+
+
+owm = OWM('ENTER API TOKEN')
+mgr = owm.weather_manager()
+
+City = input("Enter city name.. ")
+
+observation = mgr.weather_at_place(City)
+w = observation.weather
+
+print("Weather:",w.detailed_status)             
+print("Humidity:",w.humidity,"%")             
+print("Temp.",w.temperature("celsius")['temp'],"°C")           
+
+print("Press any key to exit.. ")
+input()
